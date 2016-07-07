746c85ab352ed275e6abe22854b2e9a781bcdaec
Wouter Saaltink <wouter.saaltink@gmail.com>
2016-07-07 22:18:05 +0200

    add gl and catch headers

--- download-extract-cmake.bat
+++ download-extract-cmake.bat
@@ -2,7 +2,7 @@ set CMAKE_URL=http://www.cmake.org/files/v3.2/cmake-3.2.1-win32-x86.zip
 
 cd "%~dp0"
 
-if not exist cmake.zip bin\curl --url %CMAKE_URL% > cmake.zip
+if not exist cmake.zip bin\curl -L %CMAKE_URL% > cmake.zip
 
 bin\7z  x -r -y -o. cmake.zip
 
